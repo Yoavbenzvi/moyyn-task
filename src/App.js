240d72e202ba45455b84730b8d7069e351fd7af4
Yoav <yoav.benzvi@gmail.com>
2020-05-28 16:46:20 +0200

    added a footer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import MainNavigation from './components/Navigation/MainNavigation';
 import Form from './components/Form/Form';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="">
       <MainNavigation />
    	<Form />
-   	{/*footer*/}
+   	<Footer />
     </div>  
   );
 }
