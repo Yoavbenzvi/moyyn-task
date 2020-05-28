cf5bfa628c2c98dcfb2a2ea64c85a3010d16b922
Yoav <yoav.benzvi@gmail.com>
2020-05-28 22:53:36 +0200

    edited spacer

---
+++ src/components/Form/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({text, active, handleClick}) => {
+	return(
+		<div 
+			onClick={handleClick}
+			className={`button-${!active && 'disabled' || 'default'}`}
+		>
+			{text}
+		</div>
+	)
+}
+
+export default Button;
