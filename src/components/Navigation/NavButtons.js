8ab203af97466361e350d9fc23687dc8fd126d3f
Yoav <yoav.benzvi@gmail.com>
2020-05-29 02:18:05 +0200

    creating a sidear
    -created a hamburger icon to open sidebar
    -hamburger appears on small screens only
    -created a Backdrop component to act as background for sidebar

--- src/components/Navigation/NavButtons.js
+++ src/components/Navigation/NavButtons.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navlink from './Navlink';
+import Backdrop from './SideBar/Backdrop';
 import './NavButtons.css';
 
 const NavButtons = () => {
+	const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+	const changeDrawerState = () => {
+		drawerIsOpen ? setDrawerIsOpen(false) : setDrawerIsOpen(true);
+	}
+
 	return(
-		<div className='navigation-buttons'>
-			<Navlink text={'About'}></Navlink>
-			<Navlink text={'Contact'}></Navlink>
-			<Navlink text={'Register'} classNames='link-border'></Navlink>
-			<Navlink text={'Login'} classNames='link-border'></Navlink>
-		</div>
+		<React.Fragment>
+			{drawerIsOpen && <Backdrop onClick={changeDrawerState} />}
+			<div className='navigation-buttons'>
+				<Navlink text={'About'}></Navlink>
+				<Navlink text={'Contact'}></Navlink>
+				<Navlink text={'Register'} classNames='link-border'></Navlink>
+				<Navlink text={'Login'} classNames='link-border'></Navlink>
+			</div>
+			<button onClick={changeDrawerState} className="hamburger-button">
+				<span />
+				<span />
+				<span />
+			</button>
+		</React.Fragment>
 	)
 }
 
